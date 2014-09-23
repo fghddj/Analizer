d3790368eae26af6d91edb5958cfff798108b998
fghddj <pinkbanana@gmail.com>
2014-09-23 06:26:14 -0700

    Initial Commit

---
+++ Analizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Analizer
+//
+//  Created by Blaz on 23/09/14.
+//  Copyright (c) 2014 Blaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
